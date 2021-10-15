77af8fb5be4a321c54229670c6c58ea102a9e4e3
dellevincent <dellevincent77@icloud.com>
2021-10-15 18:06:22 -0400

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.java
@@ -1,3 +1,10 @@
+/*
+*This app was created by Vincent Delle
+*Date 10/15/2021
+* Version 1.0
+* This was created out of boredom and learning purposes
+**/
+
 package com.example.passwordgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
